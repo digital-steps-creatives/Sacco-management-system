c9360dfd4600bd07e2f8e2ba47e874ce5b7b5367
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-02-01 19:00:18 +0300

    Add Laravel Aunthetication Feature

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ Route::resource('/members', 'MembersController');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
